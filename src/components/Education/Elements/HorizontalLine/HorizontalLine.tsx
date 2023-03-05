7a3e064e69b33b9d4d71d4e027c940f31026ff2b
Alexander <aleksievisa@gmail.com>
2023-03-05 08:37:55 +0300

    feat: add elements adaptive

--- src/components/Education/Elements/HorizontalLine/HorizontalLine.tsx
+++ src/components/Education/Elements/HorizontalLine/HorizontalLine.tsx
@@ -7,11 +7,15 @@ interface LineProps {
   width: string;
   delay: number
   direction: 'left' | 'right'
+  relative?: { count: number, left: number }
 }
 
-export const HorizontalLine: FC<PropsWithChildren<LineProps>> = ({ top, width, delay, direction, children }) => {
+export const HorizontalLine: FC<PropsWithChildren<LineProps>> = ({ top, width, delay, direction, children, relative }) => {
   return (
-    <div className={styles[`${direction}-line`]} style={{ top: top }}>
+    <div 
+      className={styles[`${direction}-line`]} 
+      style={{ top: top, width: `${relative?.count}px`, [direction]: `${relative?.left}px` }}
+    >
         <motion.span className={styles.start} 
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
--- src/components/Education/Elements/VerticalEducation.tsx
+++ src/components/Education/Elements/VerticalEducation.tsx
@@ -9,11 +9,11 @@ import { education } from '../../../data/education'
 const startDate = '03-22-2021'
 
 const setting = {
-  one: { delay: 1.8, width: 76, top: 17 },
-  two: { delay: 2.1, width: 76, top: 32 },
-  three: { delay: 2.8, width: 76, top: 48 },
-  four: { delay: 3.5, width: 76, top: 64 },
-  five: { delay: 4.1, width: 76, top: 79 },
+  one: { delay: 1.8, width: 76, top: 17, relative: { count: 130, left: 73 } },
+  two: { delay: 2.1, width: 55, top: 32, relative: { count: 100, left: 102 } },
+  three: { delay: 2.8, width: 76, top: 48, relative: { count: 130, left: 73 } },
+  four: { delay: 3.5, width: 65, top: 64, relative: { count: 120, left: 80 } },
+  five: { delay: 4.1, width: 65, top: 79, relative: { count: 120, left: 82 } },
 }
 
 export const VerticalEducation = () => {
@@ -29,19 +29,19 @@ export const VerticalEducation = () => {
 
   return (
     <div className={styles.container}>
-      <HorizontalLine top={`${setting.one.top}%`} width={`${setting.one.width}%`} delay={setting.one.delay} direction={'left'}>
+      <HorizontalLine relative={setting.one.relative} top={`${setting.one.top}%`} width={`${setting.one.width}%`} delay={setting.one.delay} direction={'left'}>
         <ItemEducation delay={setting.one.delay} item={education.JavaRush} direction='left' />
       </HorizontalLine>
-      <HorizontalLine top={`${setting.two.top}%`} width={`${setting.two.width}%`} delay={setting.two.delay} direction={'right'}>
+      <HorizontalLine relative={setting.two.relative} top={`${setting.two.top}%`} width={`${setting.two.width}%`} delay={setting.two.delay} direction={'right'}>
         <ItemEducation delay={setting.two.delay} item={education.rssS1} direction='right' />
       </HorizontalLine>
-      <HorizontalLine top={`${setting.three.top}%`} width={`${setting.three.width}%`} delay={setting.three.delay} direction={'left'}>
+      <HorizontalLine relative={setting.three.relative} top={`${setting.three.top}%`} width={`${setting.three.width}%`} delay={setting.three.delay} direction={'left'}>
         <ItemEducation delay={setting.three.delay} item={education.rssS2} direction='left' />
       </HorizontalLine>
-      <HorizontalLine top={`${setting.four.top}%`} width={`${setting.four.width}%`} delay={setting.four.delay} direction={'right'}>
+      <HorizontalLine relative={setting.four.relative} top={`${setting.four.top}%`} width={`${setting.four.width}%`} delay={setting.four.delay} direction={'right'}>
         <ItemEducation delay={setting.four.delay} item={education.rssReact} direction='right' />
       </HorizontalLine>
-      <HorizontalLine top={`${setting.five.top}%`} width={`${setting.five.width}%`} delay={setting.five.delay} direction={'left'}>
+      <HorizontalLine relative={setting.five.relative} top={`${setting.five.top}%`} width={`${setting.five.width}%`} delay={setting.five.delay} direction={'left'}>
         <ItemEducation delay={setting.five.delay} item={education.rssNode} direction='left' />
       </HorizontalLine>
       <div className={styles.mainLine}>
